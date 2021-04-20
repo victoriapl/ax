102ce5edc337bc6e8aa9387ece33baeb3a878997
VictoriaPl <victoriapp15@gmail.com>
2021-04-21 00:41:56 +0200

    Font size fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { HAMBURGER_MENU_ICON, NAV, TITLE } from "../../constants";
 import "./_header.css";
 
 export default function Header() {
   const history = useHistory();
+  const [selected, setSelected] = useState(history.location.pathname);
+
   return (
     <header id='header'>
       <h1 className='header-title' onClick={() => history.push("/")}>
@@ -21,7 +23,14 @@ export default function Header() {
         {NAV.map(({ title, url }, i) => (
           <h3
             key={i}
-            onClick={() => history.push(url)}
+            onClick={() => {
+              setSelected(url);
+              history.push(url);
+            }}
+            style={{
+              borderBottom: selected === url ? "2px solid white" : "",
+              textDecoration: "none",
+            }}
             className='header-link-item'>
             {title}
           </h3>
