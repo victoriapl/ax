5a274a98eabc316cef6e7766135cee06ec6f8b41
VictoriaPl <victoriapp15@gmail.com>
2021-04-24 18:25:21 +0200

    Footer allows to go home

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { useHistory } from "react-router";
 import { FOOTER_LOGO } from "../../constants";
 import "./_footer.css";
 
 export default function Footer() {
+  const history = useHistory();
   return (
     <div id='footer'>
-      <img src={FOOTER_LOGO} alt={"logo"} width={90} />
+      <img
+        src={FOOTER_LOGO}
+        alt={"logo"}
+        onClick={() => history.push("/")}
+        width={90}
+      />
     </div>
   );
 }
